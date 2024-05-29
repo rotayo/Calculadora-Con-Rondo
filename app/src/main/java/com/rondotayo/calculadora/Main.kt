1efc9207c32e00805a6a295de6ffdf858f26f1cf
rondo plus <rojasperezstewart@gmail.com>
2024-05-29 00:59:43 -0600

    Funciones aritmeticas

--- app/src/main/java/com/rondotayo/calculadora/Main.kt
+++ app/src/main/java/com/rondotayo/calculadora/Main.kt
@@ -1,5 +1,11 @@
 package com.rondotayo.calculadora
 
+import com.rondotayo.calculadora.Funciones.dividir
+import com.rondotayo.calculadora.Funciones.multiplicar
+import com.rondotayo.calculadora.Funciones.sumar
+
 fun main(){
-    
+
+    println(dividir(15,0))
 }
+
